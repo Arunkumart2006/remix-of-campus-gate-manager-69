28b7c9d354b517041a43a9b337720ba61eb13deb
Arunkumart2006 <arunkumartit2023@jkkn.ac.in>
2026-04-06 12:22:47 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,25 @@
+import { useNavigate } from "react-router-dom";
 
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to Campus Gate 🚪</h1>
+      <p>Smart Campus Management System</p>
+
+      <button
+        onClick={() => navigate("/login")}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer"
+        }}
+      >
+        Go to Login
+      </button>
+    </div>
+  );
+}
+
+export default Home;
